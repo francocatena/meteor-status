f48a4326568a84f2df9d337cdb5e47f5c72c07cb
Franco Catena <francocatena@gmail.com>
2014-07-23 13:16:09 -0300

    Move up package-tap requirement in package.js

--- package.js
+++ package.js
@@ -11,14 +11,14 @@ Package.on_use(function(api, where) {
 
   api.use(['tap-i18n'], ['client', 'server'])
 
+  api.add_files('package-tap.i18n', ['client', 'server'])
+
   api.add_files([
     'lib/status.html',
     'lib/status.js',
     'i18n/en.i18n.json',
     'i18n/es.i18n.json'
   ], 'client')
-
-  api.add_files('package-tap.i18n', ['client', 'server'])
 })
 
 Package.on_test(function(api) {
