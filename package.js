ee27ce24fb4c4d820e619fe8eda7bb1f53a92f9d
Franco Catena <francocatena@gmail.com>
2014-07-23 13:10:55 -0300

    Better package.js organization

--- package.js
+++ package.js
@@ -4,29 +4,26 @@ Package.describe({
 
 Package.on_use(function(api, where) {
   api.use([
-    'templating',
     'meteor',
-    'underscore'
+    'underscore',
+    'templating'
   ], 'client')
 
   api.use(['tap-i18n'], ['client', 'server'])
 
-  api.add_files('package-tap.i18n', ['client', 'server'])
-
   api.add_files([
     'lib/status.html',
-    'lib/status.js'
-  ], 'client')
-
-  api.add_files([
+    'lib/status.js',
     'i18n/en.i18n.json',
     'i18n/es.i18n.json'
   ], 'client')
+
+  api.add_files('package-tap.i18n', ['client', 'server'])
 })
 
 Package.on_test(function(api) {
-  api.use(['status'], 'client')
   api.use([
+    'status',
     'tinytest',
     'test-helpers'
   ], 'client')
