7eea0b4cc381b463f3c6bcd75d1bc570d63dbb6b
Franco Catena <francocatena@gmail.com>
2014-07-23 12:41:07 -0300

    Move files more coherently

--- package.js
+++ package.js
@@ -3,20 +3,33 @@ Package.describe({
 })
 
 Package.on_use(function(api, where) {
-  api.use(['templating', 'underscore'], ['client'])
+  api.use([
+    'templating',
+    'meteor',
+    'underscore'
+  ], 'client')
 
   api.use(['tap-i18n'], ['client', 'server'])
 
   api.add_files('package-tap.i18n', ['client', 'server'])
 
-  api.add_files(['status.html', 'status.js'], ['client'])
+  api.add_files([
+    'lib/status.html',
+    'lib/status.js'
+  ], 'client')
 
-  api.add_files(['i18n/en.i18n.json', 'i18n/es.i18n.json'], ['client'])
+  api.add_files([
+    'i18n/en.i18n.json',
+    'i18n/es.i18n.json'
+  ], 'client')
 })
 
 Package.on_test(function(api) {
   api.use(['status'], 'client')
-  api.use(['tinytest', 'test-helpers'], 'client')
+  api.use([
+    'tinytest',
+    'test-helpers'
+  ], 'client')
 
-  api.add_files('status_tests.js', 'client')
+  api.add_files('test/status_tests.js', 'client')
 })
