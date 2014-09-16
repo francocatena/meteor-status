0fd053f5f802444b408d878336bf7cb25a42c9ad
Franco Catena <francocatena@gmail.com>
2014-09-16 15:37:57 -0300

    Load i18n files on both client and server

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:status',
   summary: 'Display the connection status with the server',
-  version: '0.9.2',
+  version: '0.9.3',
   git: 'https://github.com/francocatena/meteor-status',
 })
 
@@ -14,14 +14,14 @@ Package.onUse(function(api) {
 
   api.use('tap:i18n@1.0.0', ['client', 'server'])
 
-  api.addFiles('package-tap.i18n', ['client', 'server'])
+  api.addFiles('i18n/package-tap.i18n', ['client', 'server'])
 
   api.addFiles('lib/status.html', 'client')
   api.addFiles('lib/retry_time.js', 'client')
   api.addFiles('lib/status.js', 'client')
 
-  api.addFiles('i18n/en.i18n.json', 'client')
-  api.addFiles('i18n/es.i18n.json', 'client')
+  api.addFiles('i18n/en.i18n.json', ['client', 'server'])
+  api.addFiles('i18n/es.i18n.json', ['client', 'server'])
 })
 
 Package.onTest(function(api) {
