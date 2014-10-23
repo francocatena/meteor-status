7b42ce44887b39b24cfe2b0d5d2a2a594d1a20bc
Franco Catena <francocatena@gmail.com>
2014-10-23 13:57:27 -0300

    Version 0.9.9 bump...

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
   name: 'francocatena:status',
   summary: 'Display the connection status with the server',
-  version: '0.9.8',
+  version: '0.9.9',
   git: 'https://github.com/francocatena/meteor-status',
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@0.9.3')
+  api.versionsFrom('METEOR@0.9.4')
 
-  api.use('deps', 'client')
-  api.use('templating', 'client')
-  api.use('underscore', 'client')
+  api.use('deps',         'client')
+  api.use('templating',   'client')
+  api.use('underscore',   'client')
   api.use('reactive-var', 'client')
 
   api.use('tap:i18n@1.0.7', ['client', 'server'])
@@ -26,14 +26,13 @@ Package.onUse(function (api) {
   api.addFiles('i18n/fr.i18n.json', ['client', 'server'])
   api.addFiles('i18n/it.i18n.json', ['client', 'server'])
 
-  api.addFiles('lib/retry_time.js', 'client')
-  api.addFiles('lib/status.js', 'client')
+  api.addFiles('lib/status.js',     'client')
 })
 
 Package.onTest(function (api) {
   api.use('francocatena:status', 'client')
-  api.use('tinytest', 'client')
-  api.use('test-helpers', 'client')
+  api.use('tinytest',            'client')
+  api.use('test-helpers',        'client')
 
   api.addFiles('test/status_tests.js', 'client')
 })
