9d85a7f90600247aace78443dc64292b882f61f0
Franco Catena <francocatena@gmail.com>
2014-10-29 14:12:17 -0300

    Lets make it 1.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'francocatena:status',
   summary: 'Display the connection status with the server',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/francocatena/meteor-status',
 })
 
