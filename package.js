6daa707d7630b3dee8b13613ffe253a6d276d758
kainlite <kainlite@gmail.com>
2015-05-08 04:12:30 -0300

    Added materialize template to package.js

--- package.js
+++ package.js
@@ -21,6 +21,7 @@ Package.onUse(function (api) {
   api.addFiles('lib/status.html',            'client')
   api.addFiles('templates/bootstrap3.html',  'client')
   api.addFiles('templates/semantic_ui.html', 'client')
+  api.addFiles('templates/materialize.html', 'client')
 
   // Always after templates
   api.addFiles('i18n/en.i18n.json', ['client', 'server'])
