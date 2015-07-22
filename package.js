17cb3b0974f1c5a5fa4f6dde2e0aebc04995905d
Franco Catena <francocatena@gmail.com>
2015-07-22 18:13:55 -0300

    Fix style and cleanup comments

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name:    'francocatena:status',
+  git:     'https://github.com/francocatena/meteor-status',
   summary: 'Display the connection status between the browser and the Meteor server',
   version: '1.2.5',
-  git:     'https://github.com/francocatena/meteor-status',
 })
 
 Package.onUse(function (api) {
